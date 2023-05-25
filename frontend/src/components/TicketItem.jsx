7841a88a3cb9cc51582f61bc4b9eeaedcec08177
ABay23 <alejandrobay23@gmail.com>
2023-05-25 19:05:58 -0400

    Titcket Element

--- frontend/src/components/TicketItem.jsx
+++ frontend/src/components/TicketItem.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const TicketItem = () => {
-  return <div>TicketItem</div>
+const TicketItem = ({ ticket }) => {
+  return (
+    <div>
+      <div>{new Date(ticket.createdAt).toLocaleString('eng-US')}</div>
+      <div>{ticket.product}</div>
+      <div className={`${ticket.status}`}>{ticket.status}</div>
+      <Link to={`/ticket/${ticket.id}`}></Link>
+    </div>
+  )
 }
 
 export default TicketItem
