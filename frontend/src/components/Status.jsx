967b5a90be01e07ff8f8cf5f46460c7fda8d2371
ABay23 <alejandrobay23@gmail.com>
2023-06-07 22:53:53 -0400

    Ticket

--- frontend/src/components/Status.jsx
+++ frontend/src/components/Status.jsx
@@ -5,9 +5,9 @@ const Status = ({ ticket }) => {
     if (ticket.status === 'new') {
       return ' bg-green-300 text-green-800 text-sm font-medium mr-2 px-2 py-0.5 rounded-full text-center'
     } else if (ticket.status === 'In Progress') {
-      return 'bg-amber-500 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full text-center'
+      return 'bg-amber-500 text-red-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full text-center'
     } else if (ticket.status === 'closed') {
-      return 'bg-red-500 text-amber-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full text-center'
+      return 'bg-red-500 text-red-200 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full text-center'
     } else {
       return 'bg-green-300 text-green-800 text-sm font-medium mr-2 px-2 py-0.5 rounded-full text-center'
     }
