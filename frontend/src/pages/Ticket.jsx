f7c2f32e656cc851c56615c155cef6ba9a828fc9
ABay23 <alejandrobay23@gmail.com>
2023-06-14 16:19:29 -0400

    Note updated

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -6,12 +6,11 @@ import {
   closeTicket,
   getTicket,
 } from '../components/features/tickets/ticketSlice'
-import { getNotes } from '../components/features/notes/noteSlice'
+import { createNote, getNotes } from '../components/features/notes/noteSlice'
 import Status from '../components/Status'
 import NoteItem from '../components/NoteItem'
 import Spinner from '../components/Spinner'
 import { FaPlus } from 'react-icons/fa'
-import { set } from 'mongoose'
 import Modal from 'react-modal'
 
 const Ticket = () => {
@@ -20,6 +19,7 @@ const Ticket = () => {
   const { ticketId } = useParams()
 
   const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [noteText, setNoteText] = useState('')
 
   const { ticket } = useSelector((state) => state.tickets)
   // console.log(ticket)
@@ -44,6 +44,17 @@ const Ticket = () => {
   const openModal = () => setModalIsOpen(true)
   const closeModal = () => setModalIsOpen(false)
 
+  // Submit Note
+  const onNoteSubmit = (e) => {
+    e.preventDefault()
+    dispatch(createNote({ ticketId, noteText }))
+      .unwrap()
+      .then(() => {
+        setNoteText('')
+        closeModal()
+        toast.success('Note added!')
+      })
+  }
   return (
     <div className='w-screen h-screen bg-gray-500'>
       <div className=' block p-40 bg-slate-400 mx-36'>
@@ -78,6 +89,26 @@ const Ticket = () => {
           <div className=' flex flex-col'>
             <h1 className=' text-xl font-bold py-6'>Add Note</h1>
             <button onClick={closeModal}>X</button>
+            <form onSubmit={onNoteSubmit}>
+              <div className=' flex flex-col'>
+                <label htmlFor='text'>Note</label>
+                <textarea
+                  className=' border-2 border-gray-500 rounded-md'
+                  name='text'
+                  id='text'
+                  value={noteText}
+                  onChange={(e) => setNoteText(e.target.value)}
+                ></textarea>
+                <div>
+                  <button
+                    className=' inline-block rounded-md bg-purple-500 px-10 py-2 font-semibold text-red-100 shadow-md duration-75 hover:bg-red-400'
+                    type='submit'
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </Modal>
 
@@ -98,5 +129,4 @@ const Ticket = () => {
     </div>
   )
 }
-
 export default Ticket
